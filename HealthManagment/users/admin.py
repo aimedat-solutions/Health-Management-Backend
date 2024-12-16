d3081175438ae313d97942a4a8ce6cbba7d4c5d6
sammedp-aat <sammed.patil@aimedatsolutions.com>
2024-12-16 14:00:41 +0530

    questions update api updated

--- HealthManagment/users/admin.py
+++ HealthManagment/users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Doctor, DietPlan,Question,Exercise, Patient, CustomUser,Option,DoctorExerciseResponse
+from .models import Doctor, DietPlan,Question,Exercise, Patient, CustomUser,Option,DoctorExerciseResponse,PatientResponse
 admin.site.register(CustomUser)
 admin.site.register(Patient)
 admin.site.register(Doctor)
@@ -9,3 +9,4 @@ admin.site.register(Exercise)
 admin.site.register(DoctorExerciseResponse)
 admin.site.register(Question)
 admin.site.register(Option)
+admin.site.register(PatientResponse)
